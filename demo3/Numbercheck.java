f9d5f0eebf0d50c85a88fe36c3e4e8e6df7d2bea
ARSHADMULLAA <arshadmulla874@gmail.com>
2025-07-23 23:03:28 +0530

    save

--- demo3/Numbercheck.java
+++ demo3/Numbercheck.java
@@ -9,9 +9,9 @@ class NumberCheck {
         if (num > 0) {
             System.out.println("The number is Positive.");
         } else if (num < 0) {
-            System.out.println("The number is Negative.");
+          System.out.println("The number is Negative.");
         } else {
-            System.out.println("The number is Zero.");
+           System.out.println("The number is Zero.");
         }
     }
 }
